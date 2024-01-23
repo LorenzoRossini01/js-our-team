d50b4fe85ba1e41d292ea3da52e5986d04f10dc0
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-01-23 14:48:21 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const resultEl = document.getElementById("result");
+
+for (let i = 0; i < team.length; i++) {
+  console.log(team[i].name, team[i].role, team[i].image);
+
+  let resultHtml = ` 
+  <div class="col-4">
+    <div class="card border-0">
+        <img src="./img/${team[i].image}" alt="" />
+        <h2 class="fs-4 my-3">${team[i].name}</h2>
+        <p class="fs-6 mb-3">${team[i].role}</p>
+    </div>
+   </div>
+  `;
+  resultEl.innerHTML += resultHtml;
+}
